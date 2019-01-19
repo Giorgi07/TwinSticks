c09fa8869e60790831f5ff2cb7de4b6d3c49fbf5
Giorgi Gaiashvili <giorgi.gaiashvili21@gmail.com>
2019-01-19 13:22:16 +0400

    Adding an UI

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadScene (string name) {
+		EditorSceneManager.LoadScene (name);
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+
+}
