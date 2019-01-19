081acdd65d15a34b056b035b8ef568c944a65396
Giorgi Gaiashvili <giorgi.gaiashvili21@gmail.com>
2019-01-19 20:59:06 +0400

    Preparing the replay system

---
+++ Assets/Scripts/ReplaySystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public struct MyKeyFrame {
+
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	MyKeyFrame(float aTime, Vector3 aPosition, Quaternion aRotation) {
+		frameTime = aTime;
+		position = aPosition;
+		rotation = aRotation;
+	}
+
+}
