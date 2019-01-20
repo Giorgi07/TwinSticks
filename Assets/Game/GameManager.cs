d7c72e1fc2623955e6ac715e284206c9b49f20ac
Giorgi Gaiashvili <giorgi.gaiashvili21@gmail.com>
2019-01-20 09:25:29 +0400

    Pausing Game and Reading from the 2nd Stick

--- Assets/Game/GameManager.cs
+++ Assets/Game/GameManager.cs
@@ -5,20 +5,53 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class GameManager : MonoBehaviour {
 
-	public bool recording = true;
+    public bool recording = true;
 
-    void Start ()
+    private float initiaFixedDelta;
+    private bool isPaused = false;
+
+    void Start()
     {
         PlayerPrefsManager.UnlockLevel(2);
         Debug.Log(PlayerPrefsManager.IsLevelUnlocked(1));
+        initiaFixedDelta = Time.fixedDeltaTime;
+
     }
 
-	// Update is called once per frame
-	void Update () {
-		if (CrossPlatformInputManager.GetButton ("Fire1")) {
-			recording = false;
-		} else {
-			recording = true;
-		}
-	}
+    // Update is called once per frame
+    void Update() {
+        if (CrossPlatformInputManager.GetButton("Fire1")) {
+            recording = false;
+        } else {
+            recording = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && isPaused)
+        {
+            isPaused = false;
+            ResumeGame();
+        } else if (Input.GetKeyDown(KeyCode.P) && !isPaused)
+        {
+            isPaused = true;
+           PauseGame();
+        }
+    }
+    
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0;
+    }
+
+    void ResumeGame ()
+    {
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = initiaFixedDelta;
+    }
+
+    void OnApplicationPause (bool pauseStatus)
+    {
+        isPaused = pauseStatus;
+    }
+
 }
